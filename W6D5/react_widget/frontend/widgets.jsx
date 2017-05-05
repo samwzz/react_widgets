6e1e8f14b4a384ad9677ae1e5ccfbdd849caf2cc
Sam Wang <sam.zz.wang@gmail.com>
2017-05-05 16:46:07 -0700

    Add clock and weather widgets

--- W6D5/react_widget/frontend/widgets.jsx
+++ W6D5/react_widget/frontend/widgets.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Clock from './clock';
+import Weather from './weather';
 
-import Root from './root';
+
+class Root extends React.Component {
+  render() {
+    return(
+      <div>
+        <Clock />
+        <Weather />
+      </div>
+    );
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root />, document.getElementById('main'));
